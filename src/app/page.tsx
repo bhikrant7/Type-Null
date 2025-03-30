2bdd1a635130fa34f72bc5c50bc727a191f4e40b
ShaswataG <gshaswata1@gmail.com>
2025-03-31 00:17:26 +0530

    style: button container padding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,8 @@ import NewNoteButton from "@/components/NewNoteButton";
 import NoteTextInput from "@/components/NoteTextInput";
 import HomeToast from "@/components/HomeToast";
 import { prisma } from "@/db/prisma";
+import AnalyseButton from "@/components/AnalyseButton";
+import { Chatbot } from "@/components/ui/chatbot";
 
 type Props = {
   searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
@@ -27,7 +29,7 @@ async function HomePage({ searchParams }: Props) {
   return (
     <div className="flex h-full items-center justify-between gap-4">
       <div className="flex flex-col items-center h-full w-3/4">
-        <div className="flex w-full max-w-4xl justify-end gap-2">
+        <div className="flex w-full max-w-4xl justify-end gap-2 pb-2">
           <AnalyseButton />
           <AskAIButton user={user} />
           <NewNoteButton user={user} />
