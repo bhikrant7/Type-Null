48bf570f89c720bedcaeeaad48cf90e82767bc30
Bhikrant Collabs <bhikrantborah@gmail.com>
2025-03-31 10:15:12 +0530

    analyse button done

--- src/components/AnalyseButtonWrapper.tsx
+++ src/components/AnalyseButtonWrapper.tsx
@@ -1,17 +1,34 @@
 "use client";
 
 import AnalyseButton from "@/components/AnalyseButton";
+import { useStore } from "../store/useStore";
+import axios from "axios";
 
-export default function AnalyseButtonWrapper({ note }: { note?: any }) {
+export default function AnalyseButtonWrapper() {
+  const API_BASE_URL =
+    process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:9000";
+
+  const { note, uploads } = useStore();
   const handleAnalyseNote = async () => {
     try {
-      if (!note) return;
+      console.log("Note data:", note); // Debugging
+      if (!note) {
+        console.warn("No note available!");
+        return;
+      }
 
-      let analyseRequestBody = {
+      const analyseRequestBody = {
         title: note.title,
-        filename: "",
+        filename: useStore.getState().uploads[0]?.filename || "",
         text: note.text,
       };
+
+      const response = await axios.post(
+        `${API_BASE_URL}/get-analysis`,
+        analyseRequestBody,
+      ).then((response)=>{
+        console.log("Result",response.data)
+      })
       console.log("analyseRequestBody: ", analyseRequestBody);
     } catch (error) {
       console.error("error: ", error);
